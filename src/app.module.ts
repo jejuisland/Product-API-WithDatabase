0ea4d3cde2c22568c30152d43f845230677f5e21
jejuisland <vailoces.jerald@gmail.com>
2022-11-16 15:26:30 +0800

    finish product CRUD API

---
+++ src/app.module.ts
@@ -0,0 +1,27 @@
+import { Module, MiddlewareConsumer } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { productModule } from './product/product.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql' as 'mysql',
+        host: configService.get<string>('DB_HOST'),
+        port: parseInt(configService.get<string>('DB_PORT')),
+        username: configService.get<string>('DB_USER'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: false,
+      }),
+      inject: [ConfigService],
+    }),
+    productModule
+  ],
+})
+export class AppModule {
+}
---
+++ src/product/product.controller.ts
@@ -0,0 +1,37 @@
+import { Body, Controller, Delete, Get, Param, Patch, Post } from "@nestjs/common";
+import { productService } from "./product.service";
+import { v4 as uuidv4 } from 'uuid';
+import { CreateProduct, UpdateProduct } from "./dto";
+
+@Controller('product')
+export class productController {
+    constructor(private service: productService) { }
+    @Post()
+    async handleCreateRecord(@Body() dto: CreateProduct) {
+        dto.uuid = uuidv4();
+        return await this.service.createRecord(dto);
+    }
+
+    @Patch(':uuid')
+    async handleUpdateRecord(@Param() data: { uuid: string }, @Body() dto: UpdateProduct) {
+        return await this.service.updateRecord(data.uuid, dto);
+    }
+
+    @Delete('')
+    async handleDeleteRecord(@Body() uuids: string[]) {
+        return await this.service.deleteRecord(uuids);
+    }
+    
+    @Get()
+    async handleList() {
+        return await this.service.getList();
+    }
+
+    @Get(':uuid')
+    async handleGetByUuid(@Param() data: { uuid: string }) {
+        return await this.service.getByUuid(data.uuid);
+    }
+
+
+
+}
